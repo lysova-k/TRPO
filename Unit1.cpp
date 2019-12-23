d18cafef22302081cf66f5c62f70dad53f4ed76a
lysova-k <153153timka@gmail.com>
2019-12-23 10:17:45 +0300

    Добавила фигуру

--- Unit1.cpp
+++ Unit1.cpp
@@ -14,3 +14,12 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 {
 }
 //---------------------------------------------------------------------------
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+	Form1->Canvas->Brush->Color = clSilver;
+	Form1->Canvas->Ellipse(0, 0, 10, 20);
+	Form1->Canvas->Brush->Color = clBlue;
+	Form1->Canvas->Ellipse(20, 20, 100, 100);
+}
+//---------------------------------------------------------------------------
+
